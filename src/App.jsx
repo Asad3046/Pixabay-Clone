d984e817137aa77efcd6730172bf537cffc20c0e
Asad <asadmohammad8259@gmail.com>
2025-12-17 23:23:13 +0530

    context created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import Images from './compenents/Images'
 import Navbar from './compenents/Navbar'
 
-// import { createContext } from "react";
-
-// const PixabayContext = createContext();
-
-// export default PixabayContext;
-
 const App = () => {
   return (
     <div>
---
+++ src/context/PixabayState.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+
+const PixabayState = (props) => {
+    const [imageData, setImageData] = useState([]);
+    const [query, setQuery] = useState("london");
+
+    const api_key = "53766989-72b2cce8b81ef226249f66125";
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const api = await fetch(`https://pixabay.com/api/?key=${api_key}&q=${query}&image_type=photo&pretty=true&per_page=50`);
+            const data = await api.json();
+            console.log(data.hits);
+            setImageData(data.hits);
+        }
+        fetchData();
+    }, [query]);
+
+    const fetchImageByCategory = async (cat) => {
+        const api = await fetch(`https://pixabay.com/api/?key=${api_key}&category=${cat}&image_type=photo&pretty=true`);
+        const data = await api.json();
+        setImageData(data.hits);
+    };
+
+    return (
+        <Context.Provider value={{ imageData, fetchImageByCategory, setQuery }}>
+            {props.children}
+        </Context.Provider>
+    );
+};
+
+export default PixabayState;
